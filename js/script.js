3501d539d254b570db0af7cac6765362411b0557
Alexey Pedchenko <ride.aer@gmail.com>
2017-04-04 13:40:55 +0300

    Исправил критерии Б2, Б9, Б28, Д14

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var navMain = document.querySelector(".main-navigation__list");
 var navToggle = document.querySelector(".main-navigation__toggle");
+var showMap = document.querySelector(".map__sedona");
 
 navMain.classList.remove("main-navigation__list--nojs");
 navMain.classList.add("main-navigation__list--closed");
 navToggle.classList.add("main-navigation__toggle--closed");
+showMap.classList.remove("map__sedona--nojs");
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-navigation__list--closed")) {
